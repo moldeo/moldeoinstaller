19916bd047984fa452fa6dbfd26ca9fefea4e2b6
Moldeo Interactive <info@moldeointeractive.com.ar>
2015-01-27 11:29:27 -0300

    Install script

---
+++ autogen_installer.sh
@@ -0,0 +1,42 @@
+
+#libmoldeo
+sudo apt-get install libmuparser-dev lua5.1-dev libtinyxml-dev libgtk2.0-dev libsdl1.2-dev libsdl2-dev libglew-dev freeglut3-dev libgle3-dev ftgl-dev libfreeimage-dev libalut-dev libgstreamer0.10-dev libboost-dev libboost-filesystem-dev libboost-thread-dev  libboost-system-dev libasio-dev autoconf-archive check
+
+#Moldeo Plugins
+sudo apt-get install libopencv-dev doxygen graphviz openjdk-7-jdk
+
+#Moldeo Director
+sudo apt-get install libgtk2.0-dev   wx-common  libwxgtk2.8-dev
+
+mkdir build
+cd build
+
+cd ../libmoldeo
+git checkout master && git pull
+./autogen_x64.sh
+sudo make install
+
+cd ../libwxmoldeocontrols
+git checkout master && git pull
+./autogen_x64.sh
+sudo make install
+
+cd ../moldeoplayer
+git checkout master && git pull
+./autogen_x64.sh
+sudo make install
+
+cd ../moldeoplugins
+git checkout master && git pull
+cd IODevices/Kinect
+./install_OpenNI.sh
+cd ../../
+./autogen_x64.sh
+sudo make install
+
+cd ../moldeodirector
+git checkout master && git pull
+./autogen_x64.sh
+sudo make install
+
+
