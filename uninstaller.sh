aec09f698367434f4b7a6972d3f112576a2420ff
Moldeo Interactive <info@moldeointeractive.com.ar>
2015-07-01 12:23:58 -0300

    Added uninstaller, checkout and pull submodules on installer.

---
+++ uninstaller.sh
@@ -0,0 +1,20 @@
+cd libmoldeo
+sudo make uninstall
+cd ../
+
+cd libwxmoldeocontrols
+sudo make uninstall
+cd ../
+
+cd moldeoplayer
+sudo make uninstall
+cd ../
+
+cd moldeoplugins
+sudo make uninstall
+cd ../
+
+cd moldeodirector
+sudo make uninstall
+cd ../
+
