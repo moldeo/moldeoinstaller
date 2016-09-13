479ba2b75aaf7e838c2b146bbfd1d8748f26d78c
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-09-13 10:57:04 -0300

    Fixing scripts for Ubuntu 16.04, Linux Mint 18

--- autogen_installer.sh
+++ autogen_installer.sh
@@ -5,10 +5,12 @@ git submodule foreach git checkout master
 git submodule foreach git pull
 
 #libmoldeo
-sudo apt-get install autoconf libtool automake libmuparser-dev lua5.1-dev libtinyxml-dev libgtk2.0-dev libsdl1.2-dev libsdl2-dev libglew-dev freeglut3-dev libgle3-dev ftgl-dev libfreeimage-dev libalut-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libboost-dev libboost-filesystem-dev libboost-thread-dev  libboost-system-dev libasio-dev autoconf-archive check
+sudo apt-get install autoconf libtool automake libmuparser-dev lua5.1-dev libtinyxml-dev libgtk2.0-dev libsdl1.2-dev libsdl2-dev libglew-dev freeglut3-dev libgle3-dev ftgl-dev libfreeimage-dev libalut-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libboost-dev libboost-filesystem-dev libboost-thread-dev  libboost-system-dev libasio-dev autoconf-archive check liblo-dev libportmidi-dev
 
 #Moldeo Plugins
-sudo apt-get install libopencv-dev doxygen graphviz openjdk-7-jdk
+sudo apt-get install libopencv-dev doxygen graphviz
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install openjdk-8-jdk
 
 #Moldeo Director
 sudo apt-get install libgtk2.0-dev   wx-common  libwxgtk2.8-dev
@@ -18,19 +20,19 @@ cd build
 
 cd ../libmoldeo
 git checkout master && git pull
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
 
 cd ../libwxmoldeocontrols
 git checkout master && git pull
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
 
 cd ../moldeoplayer
 git checkout master && git pull
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
 
@@ -39,13 +41,13 @@ git checkout master && git pull
 cd IODevices/Kinect
 ./install_OpenNI.sh
 cd ../../
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
 
 cd ../moldeodirector
 git checkout master && git pull
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
 
@@ -54,6 +56,6 @@ cd ../moldeonet
 
 cd ../moldeosamples
 git checkout master && git pull
-./autogen_x64.sh
+./autogen.sh
 make
 sudo make install
