7bde53cb75d358bc492cedaf95add0759f8ee80c
fabricio.costa@moldeointeractive.com.ar <fabricio.costa@moldeointeractive.com.ar>
2017-06-05 23:21:37 -0300

    adding include folder with dummy header

---
+++ include/moldeo.h
@@ -0,0 +1,30 @@
+#ifndef MO_MOLDEO_HEADERS
+#define MO_MOLDEO_HEADERS
+
+#include "moTypes.h"
+#include "moConsole.h"
+
+/**
+*   \mainpage
+*
+*   \if spanish
+*   Bienvenidos a la documentación de Moldeo, una plataforma de desarrollo para aplicaciones multimediales interactivas en tiempo real.
+*   \else
+*   Welcome to the Moldeo's documentation, Moldeo is a software and a development platform for interactive multimedia applications in real time.
+*   \endif
+*
+*   \code
+*
+*   \link http://www.moldeo.org/documentacion
+*
+*   \endcode
+*
+*
+*
+*   \author     Moldeo Interactive
+*   \author     Fabricio Costa Alisedo
+*
+*
+*/
+
+#endif
